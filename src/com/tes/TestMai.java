4141cf1ac0ea00e4ba79e055a45d68db1cdf2f7d
gengmingming <1874355305@qq.com>
2019-04-07 20:27:27 +0800

    第一次测试提交。。

--- src/com/tes/TestMai.java
+++ src/com/tes/TestMai.java
@@ -1,14 +1,6 @@
 package com.tes;
 
 public class TestMai {
-    @Override
-    public String toString() {
-        //8888888888
-        System.out.println("99999999999");
-        System.out.println("99999999999");
-        System.out.println("99999999999");
-        return super.toString();
-    }
 
 
     public String toString2() {
