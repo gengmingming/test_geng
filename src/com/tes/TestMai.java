18195fc91b753e0070258174d7b408fcc3d3ce56
gengmingming <1874355305@qq.com>
2019-04-07 20:25:06 +0800

    第一次测试提交。。

--- src/com/tes/TestMai.java
+++ src/com/tes/TestMai.java
@@ -19,4 +19,14 @@ public class TestMai {
         return super.toString();
     }
 
+
+    public String toString3() {
+        //8888888888
+        System.out.println("99999999999");
+        System.out.println("99999999999");
+        System.out.println("99999999999");
+        return super.toString();
+    }
+
+
 }
