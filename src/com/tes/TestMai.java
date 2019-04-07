23943fb1810b6695206055745f67186c03066acd
gengmingming <1874355305@qq.com>
2019-04-07 20:20:32 +0800

    第一次测试提交。。

--- src/com/tes/TestMai.java
+++ src/com/tes/TestMai.java
@@ -3,6 +3,9 @@ package com.tes;
 public class TestMai {
     @Override
     public String toString() {
+        //8888888888
+        System.out.println("99999999999");
+        System.out.println("99999999999");
         return super.toString();
     }
 }
